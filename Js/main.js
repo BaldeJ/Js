81419c57b48aecce19fcd74fb5132b5525c0a820
BaldeJ <67938112+BaldeJ@users.noreply.github.com>
2022-03-30 10:31:44 +0700

    Add files via upload

---
+++ Js/main.js
@@ -0,0 +1,38 @@
+function show() {
+    let age = parseInt(document.getElementById('age').value);
+    let div = document.getElementById('em');
+    let span = document.getElementById('me');
+    //let divv = document.getElementsByClassName('me');
+    let i;
+    let s = 0;
+    let arr = [];
+
+    for (let i = 0; i < age; i++) {
+        arr[i] = Math.floor(Math.random() * 10);
+    }
+    div.innerHTML=" "
+    for (i = 0; i < age; i++) {
+        div.innerHTML += arr[i]+'\n'
+    }
+
+    for (i = 0; i < age; i++) {
+        s += arr[i] + 1;
+    }
+    console.log(s)
+    span.innerHTML = "\n"+`<p>${s}</p>`;
+
+}
+function nulls() {
+    let age = parseInt(document.getElementById('age').value);
+    let div = document.getElementById('em');
+    let arr = [];
+
+    for (let i = 0; i < age; i++) {
+        arr[i] = 0;
+    }
+
+    div.innerHTML = " "
+    for (i = 0; i < age; i++) {
+        div.innerHTML += arr[i] + '\n'
+    }
+}
